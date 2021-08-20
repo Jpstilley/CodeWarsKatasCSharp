0522c9c476e80b8abb288f356e156891c6d4939f
Joey Stilley <Joey.Stilley@gmail.com>
2021-08-20 11:28:33 -0500

    Created CodeWars Kata tracker for Github

---
+++ CodeWarsKatasCSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeWarsKatasCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        // Testing 1-2-3
+        // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/csharp
+        //public static List<string> Number(List<string> lines)
+        //{
+        //    for (int i = 0; i < lines.Count; i++)
+        //    {
+        //        lines[i] = $"{i + 1}: {lines[i]}";
+        //    }
+        //    return lines;
+        //}
+
+        //  The Coupon Code
+        //  https://www.codewars.com/kata/539de388a540db7fec000642/train/csharp
+        //public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate)
+        //{
+        //    if (enteredCode == correctCode && Convert.ToDateTime(currentDate) <= Convert.ToDateTime(expirationDate))
+        //        return true;
+        //    else
+        //        return false;
+        //}
+    }
+}
